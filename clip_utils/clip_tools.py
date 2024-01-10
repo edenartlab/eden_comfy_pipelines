00c8daf15a92f9b60a2a34770a7cb2e02696e940
xander <xandersteenbruggex@gmail.com>
2024-01-10 09:52:21 -0800

    improve caching

--- clip_utils/clip_tools.py
+++ clip_utils/clip_tools.py
@@ -10,6 +10,29 @@ import torch
 import numpy as np
 from PIL import Image
 
+def find_comfy_models_dir():
+    current_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+    while True:
+        # Check if "ComfyUI/models" exists in the current path
+        target_path = os.path.join(current_path, "ComfyUI", "models")
+        if os.path.isdir(target_path):
+            return os.path.abspath(target_path)
+
+        # Move up one directory level
+        new_path = os.path.dirname(current_path)
+        if new_path == current_path:
+            # If the new path is the same, we've reached the root and didn't find the directory
+            break
+        current_path = new_path
+
+    return None
+
+# Make sure we're always downloading BLIP captioning models to the same dir:
+transformers_model_dir = os.path.join(find_comfy_models_dir(), "transformers_cache")
+os.makedirs(transformers_model_dir, exist_ok=True)
+os.environ['TRANSFORMERS_CACHE'] = transformers_model_dir
+
 def comfy_tensor_to_pil(tensor):
     # Clone the tensor and detach it from the computation graph
     tensor = tensor.clone().detach()
@@ -21,8 +44,6 @@ def comfy_tensor_to_pil(tensor):
     
     # Convert to PIL Image and return
     return Image.fromarray((tensor.cpu().numpy() * 255).astype(np.uint8))
-
-
 
 class CLIP_Interrogator:
     def __init__(self):
@@ -50,7 +71,10 @@ class CLIP_Interrogator:
             
         pil_image = comfy_tensor_to_pil(image[0])
 
-        ci = self.load_ci(clip_model_path="clip_interrogator_model")
+        clip_model_dir = os.path.join(find_comfy_models_dir(), "clip")
+        os.makedirs(clip_model_dir, exist_ok=True)
+
+        ci = self.load_ci(clip_model_path=clip_model_dir)
         if mode == "fast":
             prompt = ci.interrogate_fast(pil_image)
         else:
