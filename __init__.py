93f4062c7b86d989e0ebe2da9d4b812359cf7389
xander <xandersteenbruggex@gmail.com>
2024-02-01 16:14:05 +0100

    add logic

--- __init__.py
+++ __init__.py
@@ -3,7 +3,9 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from clip_utils.clip_tools import CLIP_Interrogator  
-from img_utils.img_nodes import IMG_padder, IMG_unpadder, IMG_scaler, Filepicker, VAEDecode_to_folder, SaveImageAdvanced, LatentTypeConversion
+from img_utils.img_nodes import *
+from logic.logic_nodes import *
+
 
 NODE_CLASS_MAPPINGS = {
     "CLIP_Interrogator": CLIP_Interrogator,
@@ -14,6 +16,14 @@ NODE_CLASS_MAPPINGS = {
     "VAEDecode_to_folder": VAEDecode_to_folder,
     "SaveImageAdvanced": SaveImageAdvanced,
     "LatentTypeConversion": LatentTypeConversion,
+    "Compare": Compare,
+    "Int": Int,
+    "Float": Float,
+    "Bool": Bool,
+    "String": String,
+    "If ANY execute A else B": IfExecute,
+    "DebugPrint": DebugPrint,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -25,6 +35,13 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "VAEDecode_to_folder": "VAEDecode_to_folder",
     "SaveImageAdvanced": "Save Image",
     "LatentTypeConversion": "LatentTypeConversion",
+    "Compare": "Compare",
+    "Int": "Int",
+    "Float": "Float",
+    "Bool": "Bool",
+    "String": "String",
+    "If ANY execute A else B": "If",
+    "DebugPrint": "DebugPrint",
 }
 
 
---
+++ logic/logic_nodes.py
@@ -0,0 +1,177 @@
+"""
+
+This code was heavily inspired by
+https://github.com/theUpsider/ComfyUI-Logic/tree/fb8897351f715ea75eebf52e74515b6d07c693b8
+
+"""
+
+
+COMPARE_FUNCTIONS = {
+    "a == b": lambda a, b: a == b,
+    "a != b": lambda a, b: a != b,
+    "a < b": lambda a, b: a < b,
+    "a > b": lambda a, b: a > b,
+    "a <= b": lambda a, b: a <= b,
+    "a >= b": lambda a, b: a >= b,
+}
+
+
+class AlwaysEqualProxy(str):
+    def __eq__(self, _):
+        return True
+
+    def __ne__(self, _):
+        return False
+
+
+class String:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"value": ("STRING", {"default": ""})},
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("STRING",)
+    FUNCTION = "execute"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def execute(self, value):
+        return (value,)
+
+
+class Int:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"value": ("INT", {"default": 0})},
+        }
+
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("INT",)
+    FUNCTION = "execute"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def execute(self, value):
+        return (value,)
+
+
+class Float:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"value": ("FLOAT", {"default": 0, "step": 0.01})},
+        }
+
+    RETURN_TYPES = ("FLOAT",)
+    RETURN_NAMES = ("FLOAT",)
+    FUNCTION = "execute"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def execute(self, value):
+        return (value,)
+
+
+class Bool:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"value": ("BOOLEAN", {"default": False})},
+        }
+
+    RETURN_TYPES = ("BOOLEAN",)
+    RETURN_NAMES = ("BOOLEAN",)
+    FUNCTION = "execute"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def execute(self, value):
+        return (value,)
+
+
+class Compare:
+    """
+    This nodes compares the two inputs and outputs the result of the comparison.
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        """
+        Comparison node takes two inputs, a and b, and compares them.
+        """
+        s.compare_functions = list(COMPARE_FUNCTIONS.keys())
+        return {
+            "required": {
+                "a": (AlwaysEqualProxy("*"), {"default": 0}),
+                "b": (AlwaysEqualProxy("*"), {"default": 0}),
+                "comparison": (s.compare_functions, {"default": "a == b"}),
+            },
+        }
+
+    RETURN_TYPES = ("BOOLEAN",)
+    RETURN_NAMES = "BOOLEAN"
+    FUNCTION = "compare"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def compare(self, a, b, comparison):
+        """
+        Compare two inputs and return the result of the comparison.
+
+        Args:
+            a (UNKNOWN): The first input.
+            b (UNKNOWN): The second input.
+            comparison (STRING): The comparison to perform. Can be one of "==", "!=", "<", ">", "<=", ">=".
+
+        Returns:
+            : The result of the comparison.
+        """
+        return (COMPARE_FUNCTIONS[comparison](a, b),)
+
+
+class IfExecute:
+    """
+    This node executes IF_TRUE if ANY is True, otherwise it executes IF_FALSE.
+    ANY can be any input, IF_TRUE and IF_FALSE can be any output.
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "ANY": (AlwaysEqualProxy("*"),),
+                "IF_TRUE": (AlwaysEqualProxy("*"),),
+                "IF_FALSE": (AlwaysEqualProxy("*"),),
+            },
+        }
+
+    RETURN_TYPES = (AlwaysEqualProxy("*"),)
+    RETURN_NAMES = "?"
+    FUNCTION = "return_based_on_bool"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def return_based_on_bool(self, ANY, IF_TRUE, IF_FALSE):
+        return (IF_TRUE if ANY else IF_FALSE,)
+
+
+class DebugPrint:
+    """
+    This node prints the input to the console.
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        """
+        Takes in any input.
+        """
+        return {"required": {"ANY": (AlwaysEqualProxy({}),)}}
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+    FUNCTION = "log_input"
+    CATEGORY = "Eden 🌱/Logic"
+
+    def log_input(self, ANY):
+        print(ANY)
+        return {}
+
+
+
