82a64c02a99ee1e28431c0d61767fc10d2047d2f
xander <xandersteenbruggex@gmail.com>
2024-08-21 17:56:22 +0200

    rename padder nodes to be unique

--- __init__.py
+++ __init__.py
@@ -15,8 +15,8 @@ from video_utils.video_interpolation import VideoFrameSelector
 
 NODE_CLASS_MAPPINGS = {
     "CLIP_Interrogator": CLIP_Interrogator,
-    "IMG_padder": IMG_padder,
-    "IMG_unpadder": IMG_unpadder,
+    "Eden_IMG_padder": IMG_padder,
+    "Eden_IMG_unpadder": IMG_unpadder,
     "IMG_scaler": IMG_scaler,
     "IMG_blender": IMG_blender,
     "ConvertToGrayscale": ConvertToGrayscale,
